4937f7511f44f9b421fa2e01a6069d5e7c3e525d
root <priveiro@gmail.com>
2023-08-21 08:16:57 -0700

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" getting todo list of users from an api  """
+import json
+import sys
+import requests
+
+
+n = sys.argv[1]
+resUser = requests.get(f'https://jsonplaceholder.typicode.com/users/{n}')
+response = resUser.json()
+resTask = requests.get('https://jsonplaceholder.typicode.com/todos')
+responseTask = resTask.json()
+done = 0
+total = 0
+doneTask = []
+for task in responseTask:
+    if task["userId"] == int(n):
+        if task["completed"] == True:
+            doneTask.append(task["title"])
+            done += 1
+        else:
+            total += 1
+
+total += done
+
+print(f"Employee {response['name']} is done with task({done}/{total})")
+for task in doneTask:
+    print(f"\t {task}")
+
+
+
+
