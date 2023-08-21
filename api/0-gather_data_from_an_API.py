2da3b37db85639f60550bd172c2467ac8c1002f7
root <priveiro@gmail.com>
2023-08-21 09:11:59 -0700

    more output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
 
     total += done
 
-    print(f"Employee {response['name']} is done with tasks({done}/{total}:)")
+    print(f"Employee {response['name']} is done with tasks({done}/{total}):")
     for task in doneTask:
         print(f"\t {task}")
